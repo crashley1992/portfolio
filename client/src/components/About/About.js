6e05f6908dbfeb3362f653b2f52f4113e05a930d
Ashley Clarke <crashley92@gmail.com>
2020-02-13 18:23:09 -0800

    added in profile image and some styling

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './about.css';
+import profile from './profile.jpg';
+import Image from 'react-bootstrap/Image';
+
 
 const About = () => {
     return (
-        <div className="container">
+    <div className="container">
+        <div className="profile-container">
+            <Image src={profile} className="profile-img"/>
             <h4 className="about-title">About Me</h4>
             <p className="about-text">I am a web developer and UW Coding bootcamp graduate with experience primarily in HTML, CSS, SQL, and Javascript. I have a strong interest in full-stack development, but am equally excited to work either on the front or back end. Additionally, I love spending my free time learning new programming skills and getting involved with the community. I love working collaboratively with a like-minded team to ensure optimal results for user-centered and technology-centered designs.</p>
         </div>
+    </div>
     )
 }
 
--- client/src/components/NavTabs/NavTabs.js
+++ client/src/components/NavTabs/NavTabs.js
@@ -16,27 +16,18 @@ const NavTabs = () => {
                 <Nav.Link href="/">Home</Nav.Link>             
                 </Nav>
             <Nav>
-                <Nav.Link href="#">About</Nav.Link>
-                </Nav>
-            <Nav>
                 <NavDropdown title="Portfolio" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/portfolio-one">Project One</NavDropdown.Item>
+                <NavDropdown.Item href="/portfolio-one">Project One: Comic Hero Finder</NavDropdown.Item>
                 <NavDropdown.Item href="/portfolio-two">Project Two</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
-                </Navbar.Collapse>
+            <div class="icon">
             {/* Nav Bar icons, used font awesome */}
-                <Nav.Link style={{ textDecoration: 'none', color: "inherit" }} target="_blank" href="https://www.linkedin.com/in/ashley-clarke-113557124/">
-                <FaLinkedin size={32} />
-                </Nav.Link>
-
-                <Nav.Link style={{ textDecoration: 'none', color: "inherit" }} target="_blank" href="https://twitter.com/CrashleyGames">
-                <FaTwitterSquare size={32}/>
-                </Nav.Link>
-
-                <Nav.Link style={{ textDecoration: 'none', color: "inherit" }} target="_blank" href="https://github.com/crashley1992">
-                <FaGithub size={32} />
-                </Nav.Link>
+                <FaLinkedin size={45} className="social-icons"/>
+                <FaTwitterSquare size={45} className="social-icons"/>
+                <FaGithub size={45} className="social-icons"/>
+                </div>
+                </Navbar.Collapse>
           </Navbar>
     )
 }
